bef80106a9daec86ab052035bc62f1912ee7450c
asya474 <51288093+asya474@users.noreply.github.com>
2023-10-02 22:08:35 +0600

    Update test_demoqa.py

--- tests_demoqa/test_demoqa.py
+++ tests_demoqa/test_demoqa.py
@@ -1,32 +1,37 @@
+import os
 from selene import browser, have, be
 from selene.support.shared import browser
 
 
-def test_submit_form(browser_open):
-    browser.should(have.title_containing('DEMOQA'))
-# first name
-    browser.element('[id="firstName"]').type('Test').press_enter()
-# last name
-    browser.element('[id="lastName"]').type('Test').press_enter()
-# email
-#    browser.element('[id="userEmail"]').type('test@test.com').press_enter()
-# gender chexbox
-    browser.element('input#gender-radio-2+.custom-control-label').click()
-# mobile number
-    browser.element('[id="userNumber"]').type('1234567890').press_enter()
-# date of birth
-#    browser.element('[id="dateOfBirthInput"]').type('b.').press_enter()
-# subjects
-#   browser.element('input#subjectsInput').type('Hello world').press_enter()
-# hobbies chexbox
-#    browser.element('input#hobbies-checkbox-1+.custom-control-label').click()
-# current address
-#    browser.element('[id="currentAddress"]').type('Hollywood').press_enter()
-# select state
-#    browser.element('#state > *:first-child > *:first-child').type('b.').press_enter()
-# select city
-#    browser.element('#city > *:first-child > *:first-child').type('b.').press_enter()
-# submit
-#    browser.element('[id="submit"]').click()
-# submit form
-    browser.element('#example-modal-sizes-title-lg').should(be.visible)
+def test_registration_form(browser_management):
+    browser.open("/automation-practice-form")
+    browser.element("#firstName").should(be.visible).type('Test')
+    browser.element("#lastName").should(be.visible).type('Test')
+    browser.element("#userEmail").should(be.visible).type('test@test.com')
+    browser.element("input#gender-radio-2+.custom-control-label").should(be.visible).click()
+    browser.element("#userNumber").should(be.visible).type("1234567890")
+    browser.element("#dateOfBirthInput").should(be.visible).click()
+    browser.element(".react-datepicker__month-select").should(be.visible).click()
+    browser.element(".react-datepicker__month-select > option:nth-child(5)").should(be.visible).click()
+    browser.element(".react-datepicker__year-select").should(be.visible).click()
+    browser.element(".react-datepicker__year-select > option:nth-child(20)").should(be.visible).click()
+    browser.element(".react-datepicker__day.react-datepicker__day--012").should(be.visible).click()
+    browser.element("input#subjectsInput").should(be.visible).type('Hello world').press_enter()
+    browser.element("input#hobbies-checkbox-1+.custom-control-label").should(be.visible).click()
+    browser.element("#uploadPicture").should(be.visible).type(os.path.abspath("pictures/character.png"))
+    browser.element("currentAddress").should(be.visible).type("Hollywood")
+    browser.element("#state > *:first-child > *:first-child").should(be.visible).type("NCR").press_enter()
+    browser.element("#city > *:first-child > *:first-child").should(be.visible).type("Delhi").press_enter()
+    browser.element("#submit").should(be.visible).click()
+    browser.all("#example-modal-sizes-title-lg").should(have.texts(
+    "Test Test",
+    "test@test.com",
+    "Female",
+    "1234567890",
+    "12 May 1919",
+    "Hello world",
+    "Sports",
+    "character.png",
+    "Hollywood",
+    "NCR Delhi"))
+    browser.element("#closeLargeModal").should(be.visible).click()
