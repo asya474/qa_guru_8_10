7657d1b7fff96b6fbce4aad663fc1663e362ef5d
asya474 <51288093+asya474@users.noreply.github.com>
2023-10-02 22:03:26 +0600

    Update conftest.py

--- tests_demoqa/conftest.py
+++ tests_demoqa/conftest.py
@@ -3,11 +3,11 @@ from selene import browser
 
 
 @pytest.fixture(scope="function", autouse=True)
-def browser_open():
-    browser.config.window_width = 400
-    browser.config.window_height = 620
-    browser.open('https://demoqa.com/automation-practice-form')
+def browser_management():
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield
 
-    ...
+    browser.quit()
