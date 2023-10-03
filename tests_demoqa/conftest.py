c2907288fbf42a1ec54acad15e2e7f0c1ade2946
asya474 <51288093+asya474@users.noreply.github.com>
2023-10-03 19:17:22 +0600

    Update conftest.py

--- tests_demoqa/conftest.py
+++ tests_demoqa/conftest.py
@@ -4,9 +4,10 @@ from selene import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_management():
-    browser.config.base_url = "https://demoqa.com"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.timeout = 6.0
 
     yield
 
