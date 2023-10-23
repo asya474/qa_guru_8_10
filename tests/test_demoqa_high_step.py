68c6ca6790090f8dfaf9ae50ea7c128374679c1d
asya474 <asya474@gmail.com>
2023-10-23 19:15:24 +0700

    first commit

---
+++ tests/test_demoqa_high_step.py
@@ -0,0 +1,23 @@
+from pages.registration_page import RegistrationPage
+from data.users import User
+
+
+def test_registration_form(browser_setup):
+    user = User('Test',
+                'Test',
+                'test@test.com',
+                'Male',
+                '1234567890',
+                '2001',
+                'May',
+                '15',
+                'Computer Science',
+                'Reading',
+                'character.png',
+                'Sugar Palace, a candy store in Ponyville',
+                'NCR',
+                'Delhi')
+    registration_page = RegistrationPage()
+    registration_page.open()
+    registration_page.register(user=user)
+    registration_page.should_have_registered(user)
